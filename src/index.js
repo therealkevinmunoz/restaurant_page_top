3c0b49538316ff64ed1d651ab0357cd077d9fe8a
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-09-18 22:58:11 -0700

    Create index.html, add images and fonts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+import './styles.css';
+import pageBanner from './images/Restaurant_Page_Banner.png';
+
 console.log("hi");
+
+function component() {
+    const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+ 
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = pageBanner;
+ 
+   element.appendChild(myIcon);
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());

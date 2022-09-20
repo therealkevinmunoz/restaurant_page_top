20d1c45694230228983c9446ed9b80f7838eb444
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-09-19 23:32:01 -0700

    Add menu and contact page with complimentary images

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import './styles.css';
 import pageBanner from './images/Restaurant_Page_Banner.png';
+import menuItemBSB from './images/Restaurant_Page_Menu_Item_BSB.png';
 
 console.log("hi");
-
-function component() {
-    const element = document.createElement('div');
- 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
- 
-   // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = pageBanner;
- 
-   element.appendChild(myIcon);
- 
-    return element;
-  }
- 
-  document.body.appendChild(component());
